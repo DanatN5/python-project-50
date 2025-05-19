2ab42eb5e54715e4d24a0944dbab64f96e17e051
Danat Nurmambetov <danat-1998@mail.ru>
2025-05-20 01:23:28 +0300

    To push

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,20 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="""
+usage: gendiff [-h] first_file second_file
+
+Compares two configuration files and shows a difference.
+
+positional arguments:
+  first_file
+  second_file
+
+options:
+  -h, --help            show this help message and exit""")
+
 def main():
     print('Yo!')
+    
 
 
 if __name__ == "__main__":
