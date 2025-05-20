42e8ccadd76097bc34e299b6e3ec6579bc21a4be
Danat Nurmambetov <danat-1998@mail.ru>
2025-05-20 03:56:00 +0300

    Add Makefile

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,19 +1,12 @@
 import argparse
 
-parser = argparse.ArgumentParser(description="""
-usage: gendiff [-h] first_file second_file
+parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference')
 
-Compares two configuration files and shows a difference.
-
-positional arguments:
-  first_file
-  second_file
-
-options:
-  -h, --help            show this help message and exit""")
+parser.add_argument('first_file')
+parser.add_argument('second_file')
 
 def main():
-    print('Yo!')
+    pass
     
 
 
