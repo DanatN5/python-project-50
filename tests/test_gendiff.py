c4052511e6f93b951d26550501b45669a45e031f
Danat Nurmambetov <danat-1998@mail.ru>
2025-05-27 17:49:07 +0300

    Fix flat_diff.txt

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,8 +2,9 @@ from gendiff import generate_diff, load_files
 
 
 def test_gendiff():
-    data1, data2 = load_files('tests/test_data/file1.json', 'tests/test_data/file2.json')
-    with open('tests/test_data/flat_diff.py') as file:
+    data1, data2 = load_files('tests/test_data/file1.json',
+                              'tests/test_data/file2.json')
+    with open('tests/test_data/flat_diff.txt') as file:
         result = file.read()
     
     assert generate_diff(data1, data2) == result
