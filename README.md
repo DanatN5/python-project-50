73c7278c7be62257c2522416245b3f2a3744793f
Danat Nurmambetov <danat-1998@mail.ru>
2025-06-04 09:22:34 +0300

    Fix README.md, add json_format tests

--- README.md
+++ README.md
@@ -5,6 +5,37 @@
 [![Python CI](https://github.com/DanatN5/python-project-50/actions/workflows/build.yml/badge.svg)](https://github.com/DanatN5/python-project-50/actions/workflows/build.yml)
 [![Coverage](https://sonarcloud.io/api/project_badges/measure?project=DanatN5_python-project-50&metric=coverage)](https://sonarcloud.io/summary/new_code?id=DanatN5_python-project-50)
 
+# gendiff
 
-Демонстрация работы программы:
-  https://asciinema.org/a/eFkqRBiYWPed2i0t1thd402pV
+> Генератор различий между двумя конфигурационными файлами
+
+## Описание:
+
+'gendiff' - утилита командной строки, которая сравнивает два файла (JSON, YAML) и показывает различия в трех форматах: 'stylish', 'plain', 'json'
+
+# Установка:
+
+``` 
+git clone git@github.com:DanatN5/python-project-50.git
+```
+
+````
+cd python-project-50
+````
+
+`````
+uv build
+``````
+
+````````
+uv tool install dist/*.whl
+````````
+
+## Поддерживаемые форматы
+
+#### - JSON (.json)
+
+#### - YAML (.yaml, .yml)
+
+# Демонстрация работы программы:
+  https://asciinema.org/a/vwViVqg2xKasfhiyrp0LvFJPA
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -14,6 +14,9 @@ with open('tests/test_data/diff.txt') as file:
 with open('tests/test_data/plain_diff.txt') as file:
     gendiff_result_plain = file.read()
 
+with open('tests/test_data/json_diff.txt') as file:
+    gendiff_result_json = file.read()
+
 
 def test_gendiff_flat_json():
     data1, data2 = parse_files('tests/test_data/file1.json',
@@ -41,6 +44,22 @@ def test_gendiff_yaml():
                               'tests/test_data/file4.yaml')
         
     assert generate_diff(data1, data2) == gendiff_result
+
+
+def test_gendiff_json_format_json():
+    data1, data2 = parse_files('tests/test_data/file3.json',
+                              'tests/test_data/file4.json')
+     
+    assert generate_diff(data1, data2, 
+                         format_name='json') == gendiff_result_json
+
+
+def test_gendiff_json_format_yaml():
+    data1, data2 = parse_files('tests/test_data/file3.yaml',
+                              'tests/test_data/file4.yaml')
+        
+    assert generate_diff(data1, data2, 
+                         format_name='json') == gendiff_result_json
 
 
 def test_gendiff_plain_json():
