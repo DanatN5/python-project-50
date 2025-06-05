59b4d72ed609005c092a1814b310d13dcc3ad47e
Danat Nurmambetov <danat-1998@mail.ru>
2025-06-05 12:44:36 +0300

    Fix tests/

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,3 +1,4 @@
+import pytest
 import json
 from pathlib import Path
 
@@ -5,82 +6,34 @@ import yaml
 
 from gendiff import generate_diff, parse_files
 
-with open('tests/test_data/flat_diff.txt') as file:
-    gendiff_result_flat = file.read()
-
-with open('tests/test_data/diff.txt') as file:
-    gendiff_result = file.read()
-
-with open('tests/test_data/plain_diff.txt') as file:
-    gendiff_result_plain = file.read()
-
-with open('tests/test_data/json_diff.txt') as file:
-    gendiff_result_json = file.read()
-
-
-def test_gendiff_flat_json():
-    data1, data2 = ('tests/test_data/file1.json',
-                              'tests/test_data/file2.json')
-     
-    assert generate_diff(data1, data2) == gendiff_result_flat
-
-
-def test_gendiff_flat_yaml():
-    data1, data2 = ('tests/test_data/file1.yaml',
-                              'tests/test_data/file2.yml')
-        
-    assert generate_diff(data1, data2) == gendiff_result_flat
-
-
-def test_gendiff_json():
-    data1, data2 = ('tests/test_data/file3.json',
-                              'tests/test_data/file4.json')
-     
-    assert generate_diff(data1, data2) == gendiff_result
-
-
-def test_gendiff_yaml():
-    data1, data2 = ('tests/test_data/file3.yaml',
-                              'tests/test_data/file4.yaml')
-        
-    assert generate_diff(data1, data2) == gendiff_result
-
-
-def test_gendiff_json_format_json():
-    data1, data2 = ('tests/test_data/file3.json',
-                              'tests/test_data/file4.json')
-     
-    assert generate_diff(data1, data2, 
-                         format_name='json') == gendiff_result_json
-
-
-def test_gendiff_json_format_yaml():
-    data1, data2 = ('tests/test_data/file3.yaml',
-                              'tests/test_data/file4.yaml')
-        
-    assert generate_diff(data1, data2, 
-                         format_name='json') == gendiff_result_json
-
-
-def test_gendiff_plain_json():
-    data1, data2 = ('tests/test_data/file3.json',
-                              'tests/test_data/file4.json')
-     
-    assert generate_diff(data1, data2, 
-                         format_name='plain') == gendiff_result_plain
-
-
-def test_gendiff_plain_yaml():
-    data1, data2 = ('tests/test_data/file3.yaml',
-                              'tests/test_data/file4.yaml')
-        
-    assert generate_diff(data1, data2, 
-                         format_name='plain') == gendiff_result_plain
+@pytest.mark.parametrize(
+    'file1, file2, format_name, expected',
+    [
+        ('tests/test_data/file11.json', 'tests/test_data/file21.json',
+         'stylish', 'tests/test_data/result_stylish.txt'),
+         ('tests/test_data/file11.json', 'tests/test_data/file21.json',
+         'plain', 'tests/test_data/result_plain.txt'),
+         ('tests/test_data/file11.yml', 'tests/test_data/file21.yml',
+         'stylish', 'tests/test_data/result_stylish.txt'),
+         ('tests/test_data/file11.yml', 'tests/test_data/file21.yml',
+         'plain', 'tests/test_data/result_plain.txt'),
+         ('tests/test_data/file3.json', 'tests/test_data/file4.json',
+         'json', 'tests/test_data/json_diff.txt'),
+         ('tests/test_data/file3.yaml', 'tests/test_data/file4.yaml',
+         'json', 'tests/test_data/json_diff.txt'),
+    ]
+)
+def test_generate_diff(file1,file2, format_name, expected):
+    with open(expected) as file:
+        expected_result = file.read()
+    result = generate_diff(file1, file2, format_name=format_name)
+    
+    assert result == expected_result
 
 
 def test_parse_files_json():
-    path_json1 = 'tests/test_data/file1.json'
-    path_json2 = 'tests/test_data/file2.json'
+    path_json1 = 'tests/test_data/file3.json'
+    path_json2 = 'tests/test_data/file4.json'
     with open(Path(path_json1)) as data1, open(Path(path_json2)) as data2:
         file1 = json.load(data1)
         file2 = json.load(data2)
@@ -89,8 +42,8 @@ def test_parse_files_json():
 
 
 def test_parse_files_yaml():
-    path_yaml1 = 'tests/test_data/file1.yaml'
-    path_yaml2 = 'tests/test_data/file2.yml'
+    path_yaml1 = 'tests/test_data/file3.yaml'
+    path_yaml2 = 'tests/test_data/file4.yaml'
     with open(Path(path_yaml1)) as data1, open(Path(path_yaml2)) as data2:
         file1 = yaml.safe_load(data1)
         file2 = yaml.safe_load(data2)
