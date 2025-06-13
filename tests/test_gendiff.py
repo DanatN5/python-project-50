724bb830987b32b7706bb35680663f62d2ef2bcd
Danat Nurmambetov <danat-1998@mail.ru>
2025-06-13 08:53:16 +0300

    Fix test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -7,13 +7,17 @@ from gendiff import generate_diff
     'file1, file2, format_name, expected',
     [
         ('tests/test_data/file1.json', 'tests/test_data/file2.json',
-         'stylish', 'tests/test_data/result_stylish.txt'),
+         'stylish', 'tests/test_data/expected_result_json.txt'),
         ('tests/test_data/file1.json', 'tests/test_data/file2.json',
-         'plain', 'tests/test_data/result_plain.txt'),
-        ('tests/test_data/file1.yml', 'tests/test_data/file2.yml',
-         'stylish', 'tests/test_data/result_stylish.txt'),
-        ('tests/test_data/file1.yml', 'tests/test_data/file2.yml',
-         'plain', 'tests/test_data/result_plain.txt'),
+         'plain', 'tests/test_data/expected_result_plain.txt'),
+        ('tests/test_data/file1.yaml', 'tests/test_data/file2.yaml',
+         'stylish', 'tests/test_data/expected_result_yaml.txt'),
+        ('tests/test_data/file1.yaml', 'tests/test_data/file2.yaml',
+         'plain', 'tests/test_data/expected_result_plain.txt'),
+        ('tests/test_data/file1.json', 'tests/test_data/file2.json',
+         'json', 'tests/test_data/expected_result_json_format.txt'),
+        ('tests/test_data/file1.yaml', 'tests/test_data/file2.yaml',
+         'json', 'tests/test_data/expected_result_json_format.txt'),
     ]
 )
 def test_generate_diff(file1, file2, format_name, expected):
@@ -21,4 +25,4 @@ def test_generate_diff(file1, file2, format_name, expected):
         expected_result = file.read()
     result = generate_diff(file1, file2, format_name=format_name)
     
-    assert result == expected_result
+    assert result.strip() == expected_result.strip()
